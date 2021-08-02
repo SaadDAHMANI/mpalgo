24ad63a0c98bb58aaf53f3a25c26ffb4d37e3a35
SaadDAHMANI <tahatatakor7@gmail.com>
2021-08-02 21:39:18 +0100

    Initialize variables.
    Signed-off-by: SaadDAHMANI <tahatatakor7@gmail.com>

--- src/mpa.rs
+++ src/mpa.rs
@@ -8,7 +8,7 @@ fn mpa (searchagents_no : usize , max_iter : usize, lb : f64, ub : f64, dim : us
      println!("computation with : n= {}, d= {}, kmax= {}, lb= {}, ub ={} of ",searchagents_no, dim, max_iter, lb,ub);
      
      let mut top_predator_pos = vec![0.0f64; dim]; // Top_predator_pos=zeros(1,dim);  the best solution :
-     let mut top_predator_fit : f64 = f64::MAX;  // Top_predator_fit=inf;  the best fitness
+     let mut top_predator_fit : f64 = 0.0f64; //f64::MAX;  // Top_predator_fit=inf;  the best fitness
      let mut convergence_curve = vec![0.0f64; max_iter]; //Convergence_curve=zeros(1,Max_iter); the best chart
      let mut stepsize = vec![vec![0.064; dim]; searchagents_no];//stepsize=zeros(SearchAgents_no,dim);
      
@@ -17,9 +17,17 @@ fn mpa (searchagents_no : usize , max_iter : usize, lb : f64, ub : f64, dim : us
           *fit = f64::MAX;
      }
 
-     //Prey=initialization(SearchAgents_no,dim,ub,lb);
-      let mut prey = initialization(searchagents_no, dim, lb, ub);
-      write_matrix(&prey, String::from("prey"));    
+     
+      let mut prey = initialization(searchagents_no, dim, lb, ub); //Prey=initialization(SearchAgents_no,dim,ub,lb);
+      //write_matrix(&prey, String::from("prey"));  
+      
+      let xmin = repmat(searchagents_no, dim, lb); //Xmin=repmat(ones(1,dim).*lb,SearchAgents_no,1);
+      let xmax = repmat(searchagents_no, dim, ub); //Xmax=repmat(ones(1,dim).*ub,SearchAgents_no,1); 
+
+      let mut iter =0;  
+      let FADs = 0.2;
+      let p = 0.5;
+
 
 
      return top_predator_fit;   
@@ -39,6 +47,16 @@ fn initialization(searchagents_no : usize, dim : usize, lb : f64, ub : f64)-> Ve
       }    
       
       positions
+}
+
+fn repmat(n: usize ,d : usize, value : f64) -> Vec<Vec<f64>> {
+      let mut matrix = vec![vec![0.0f64; d]; n];
+          for i in 0..n {
+               for j in 0..d {
+                    matrix[i][j] = value;
+               }
+          }
+      matrix
 }
 
 
