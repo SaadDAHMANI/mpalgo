12e78cd10fdd0052fa1e7cc056c5f5eb9a012276
SaadDAHMANI <tahatatakor7@gmail.com>
2021-08-02 16:46:31 +0100

    Initilization
    Signed-off-by: SaadDAHMANI <tahatatakor7@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+
+//  Marine Predators Algorithm
+//  paper:
+//  A. Faramarzi, M. Heidarinejad, S. Mirjalili, A.H. Gandomi, 
+//  Marine Predators Algorithm: A Nature-inspired Metaheuristic
+//  Expert Systems with Applications
+//  DOI: doi.org/10.1016/j.eswa.2020.113377
+//  Original Matlab code : https://github.com/afshinfaramarzi/Marine-Predators-Algorithm
+
+//Re-implemented in Rust by SaadDAHMANI <sd.dahmani2000@gmail.com; s.dahmani@univ-bouira.dz>
+
+
 fn main() {
     println!("Marine Predators Algorithm (MPA)");
 }
